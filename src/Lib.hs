970bdd3adf287a3dedb6d4a7dd0bff92e93d1435
William Yager <will@yager.io>
2022-09-13 13:26:08 -0400

    ormolu

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,166 +1,247 @@
-{-# LANGUAGE OverloadedStrings, DerivingVia, LambdaCase, DeriveGeneric, DeriveAnyClass #-}
-module Lib
-    (ArtCommand(..), ArtPoll_(..), ArtPollReply_(..), ArtDMX_(..)
-     
-    ) where
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-import Data.Serialize (Serialize, put, get, getByteString, putByteString,
-    getWord8, getWord16le, putWord16le, putWord8, putWord16be, getWord16be)
-import Data.ByteString (ByteString)
-import qualified Data.ByteString as BS
-import Data.Word (Word8, Word16, Word32, Word64)
-import GHC.Generics
+module Lib
+  ( ArtCommand (..),
+    ArtPoll_ (..),
+    ArtPollReply_ (..),
+    ArtDMX_ (..),
+  )
+where
+
 import Control.Monad (guard)
 import Data.Bits (Bits, testBit)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import Data.Serialize
+  ( Serialize,
+    get,
+    getByteString,
+    getWord16be,
+    getWord16le,
+    getWord8,
+    put,
+    putByteString,
+    putWord16be,
+    putWord16le,
+    putWord8,
+  )
+import Data.Word (Word16, Word32, Word64, Word8)
+import GHC.Generics
 
+newtype U16LE = U16LE {u16le :: Word16} deriving (Show)
 
-
-newtype U16LE = U16LE {u16le :: Word16} deriving Show
 instance Serialize U16LE where
-    put = putWord16le . u16le
-    get = U16LE <$> getWord16le
+  put = putWord16le . u16le
+  get = U16LE <$> getWord16le
 
-newtype U16BE = U16BE {u16be :: Word16} deriving Show
+newtype U16BE = U16BE {u16be :: Word16} deriving (Show)
+
 instance Serialize U16BE where
-    put = putWord16be . u16be
-    get = U16BE <$> getWord16be
+  put = putWord16be . u16be
+  get = U16BE <$> getWord16be
 
+newtype Sequence = Sequence Word8
+  deriving (Serialize) via Word8
+  deriving (Show)
 
-newtype Sequence = Sequence Word8 deriving Serialize via Word8 deriving Show
-newtype Physical = Physical Word8 deriving Serialize via Word8 deriving Show
-newtype Flags = Flags Word8 deriving (Serialize, Eq, Bits, Num) via Word8 deriving Show
+newtype Physical = Physical Word8
+  deriving (Serialize) via Word8
+  deriving (Show)
+
+newtype Flags = Flags Word8
+  deriving (Serialize, Eq, Bits, Num) via Word8
+  deriving (Show)
+
 flagsTargeted :: Flags -> Bool
 flagsTargeted = (`testBit` 5)
-newtype UBEAVersion = UBEAVersion Word8 deriving Serialize via Word8 deriving Show
-newtype DiagPriority = DiagPriority Word8 deriving (Serialize, Num) via Word8 deriving Show
-newtype Universe = Universe Word16 deriving Serialize via U16LE deriving Show
-newtype TargetPortAddr = TargetPortAddr Word16 deriving Serialize via U16BE deriving Show
 
-newtype Data = Data ByteString deriving Show
+newtype UBEAVersion = UBEAVersion Word8
+  deriving (Serialize) via Word8
+  deriving (Show)
+
+newtype DiagPriority = DiagPriority Word8
+  deriving (Serialize, Num) via Word8
+  deriving (Show)
+
+newtype Universe = Universe Word16
+  deriving (Serialize) via U16LE
+  deriving (Show)
+
+newtype TargetPortAddr = TargetPortAddr Word16
+  deriving (Serialize) via U16BE
+  deriving (Show)
+
+newtype Data = Data ByteString deriving (Show)
+
 instance Serialize Data where
-    put (Data bs) = do
-        putWord16be $ fromIntegral (BS.length bs)
-        putByteString bs
-    get = do
-        len <- getWord16be
-        Data <$> getByteString (fromIntegral len)
-
+  put (Data bs) = do
+    putWord16be $ fromIntegral (BS.length bs)
+    putByteString bs
+  get = do
+    len <- getWord16be
+    Data <$> getByteString (fromIntegral len)
 
 data IPv4 = IPv4 Word8 Word8 Word8 Word8 deriving (Generic, Serialize, Show)
+
 data MAC = MAC Word8 Word8 Word8 Word8 Word8 Word8 deriving (Generic, Serialize, Show)
+
 data UID = UID Word8 Word8 Word8 Word8 Word8 Word8 deriving (Generic, Serialize, Show)
+
 data PortTypes = PortTypes Word8 Word8 Word8 Word8 deriving (Generic, Serialize, Show)
+
 data GoodInput = GoodInput Word8 Word8 Word8 Word8 deriving (Generic, Serialize, Show)
+
 data GoodOutput = GoodOutput Word8 Word8 Word8 Word8 deriving (Generic, Serialize, Show)
+
 data SwIn = SwIn Word8 Word8 Word8 Word8 deriving (Generic, Serialize, Show)
+
 data SwOut = SwOut Word8 Word8 Word8 Word8 deriving (Generic, Serialize, Show)
+
 data Filler = Filler Word64 Word64 Word64 Word32 Word8 deriving (Generic, Serialize, Show)
 
-data Port6454 = Port6454 deriving Show
+data Port6454 = Port6454 deriving (Show)
 
 instance Serialize Port6454 where
-    put Port6454 = putWord16le 0x1936
-    get = do
-        0x1936 <- getWord16le
-        return Port6454
+  put Port6454 = putWord16le 0x1936
+  get = do
+    0x1936 <- getWord16le
+    return Port6454
 
-newtype VersInfo = VersInfo Word16 deriving Serialize via U16BE deriving Show
-newtype OEM = OEM Word16 deriving Serialize via U16BE deriving Show
-newtype ESTA = ESTA Word16 deriving Serialize via U16BE deriving Show
-newtype NumPorts = NumPorts Word16 deriving Serialize via U16BE deriving Show
+newtype VersInfo = VersInfo Word16
+  deriving (Serialize) via U16BE
+  deriving (Show)
+
+newtype OEM = OEM Word16
+  deriving (Serialize) via U16BE
+  deriving (Show)
+
+newtype ESTA = ESTA Word16
+  deriving (Serialize) via U16BE
+  deriving (Show)
+
+newtype NumPorts = NumPorts Word16
+  deriving (Serialize) via U16BE
+  deriving (Show)
 
 data Switch = Switch Word8 Word8 deriving (Generic, Serialize, Show)
-newtype Status  = Status Word8 deriving Serialize via Word8 deriving Show
-newtype AcnPriority  = AcnPriority Word8 deriving Serialize via Word8 deriving Show
-newtype SwMacro = SwMacro Word8 deriving Serialize via Word8 deriving Show
-newtype SwRemote = SwRemote Word8 deriving Serialize via Word8 deriving Show
-newtype Spare = Spare Word8 deriving Serialize via Word8 deriving Show
-newtype Style = Style Word8 deriving Serialize via Word8 deriving Show
-newtype BindIndex = BindIndex Word8 deriving Serialize via Word8 deriving Show
 
-newtype ShortName = ShortName {getShortName :: ByteString } deriving Show
+newtype Status = Status Word8
+  deriving (Serialize) via Word8
+  deriving (Show)
+
+newtype AcnPriority = AcnPriority Word8
+  deriving (Serialize) via Word8
+  deriving (Show)
+
+newtype SwMacro = SwMacro Word8
+  deriving (Serialize) via Word8
+  deriving (Show)
+
+newtype SwRemote = SwRemote Word8
+  deriving (Serialize) via Word8
+  deriving (Show)
+
+newtype Spare = Spare Word8
+  deriving (Serialize) via Word8
+  deriving (Show)
+
+newtype Style = Style Word8
+  deriving (Serialize) via Word8
+  deriving (Show)
+
+newtype BindIndex = BindIndex Word8
+  deriving (Serialize) via Word8
+  deriving (Show)
+
+newtype ShortName = ShortName {getShortName :: ByteString} deriving (Show)
+
 instance Serialize ShortName where
-    get = ShortName <$> getByteString 18
-    put = putByteString . getShortName
+  get = ShortName <$> getByteString 18
+  put = putByteString . getShortName
 
-newtype LongName = LongName {getLongName :: ByteString } deriving Show
+newtype LongName = LongName {getLongName :: ByteString} deriving (Show)
+
 instance Serialize LongName where
-    get = LongName <$> getByteString 64
-    put = putByteString . getLongName
+  get = LongName <$> getByteString 64
+  put = putByteString . getLongName
 
-newtype NodeReport = NodeReport {getNodeReport :: ByteString } deriving Show
+newtype NodeReport = NodeReport {getNodeReport :: ByteString} deriving (Show)
+
 instance Serialize NodeReport where
-    get = NodeReport <$> getByteString 64
-    put = putByteString . getNodeReport
+  get = NodeReport <$> getByteString 64
+  put = putByteString . getNodeReport
 
-data ArtPollReply_ = ArtPollReply_ IPv4 Port6454 VersInfo Switch OEM UBEAVersion Status ESTA ShortName LongName NodeReport NumPorts PortTypes GoodInput GoodOutput AcnPriority SwMacro SwRemote Spare Spare Spare Style MAC IPv4  BindIndex Status GoodOutput Status UID Filler deriving (Generic, Serialize, Show)
+data ArtPollReply_ = ArtPollReply_ IPv4 Port6454 VersInfo Switch OEM UBEAVersion Status ESTA ShortName LongName NodeReport NumPorts PortTypes GoodInput GoodOutput AcnPriority SwMacro SwRemote Spare Spare Spare Style MAC IPv4 BindIndex Status GoodOutput Status UID Filler deriving (Generic, Serialize, Show)
 
 data ArtPoll_ = ArtPoll_ Flags DiagPriority (Maybe (TargetPortAddr, TargetPortAddr)) deriving (Show)
+
 instance Serialize ArtPoll_ where
-    get = do
-        flags <- get
-        dp <- get
-        target <- if flagsTargeted flags
-            then Just <$> ((,) <$> get <*> get)
-            else return Nothing
-        return $ ArtPoll_ flags dp target
-    -- TODO: Break out flags so we can't represent a thing with
-    -- targeted = false and targets = Just ...
-    put (ArtPoll_ f dp t) = put f >> put dp >> mapM_ (\(a,b) -> put a >> put b) t
-        
-        
+  get = do
+    flags <- get
+    dp <- get
+    target <-
+      if flagsTargeted flags
+        then Just <$> ((,) <$> get <*> get)
+        else return Nothing
+    return $ ArtPoll_ flags dp target
+
+  -- TODO: Break out flags so we can't represent a thing with
+  -- targeted = false and targets = Just ...
+  put (ArtPoll_ f dp t) = put f >> put dp >> mapM_ (\(a, b) -> put a >> put b) t
 
 data ArtDMX_ = ArtDMX_ Sequence Physical Universe Data deriving (Generic, Serialize, Show)
 
-data ArtCommand = 
-    -- A poll command | used to discover devices on the network
-    ArtPoll  ArtPoll_ |
-
-    -- A reply to the poll command | it contains device status information
-    ArtPollReply  ArtPollReply_ |
-
-
-    -- An ArtDmx data packet. Used to send actual data to a node in the network
+data ArtCommand
+  = -- A poll command | used to discover devices on the network
+    ArtPoll ArtPoll_
+  | -- A reply to the poll command | it contains device status information
+    ArtPollReply ArtPollReply_
+  | -- An ArtDmx data packet. Used to send actual data to a node in the network
     ArtDMX ArtDMX_
-    deriving Show
+  deriving (Show)
 
-data Proto = V14 deriving Show
+data Proto = V14 deriving (Show)
+
 instance Serialize Proto where
-    put V14 = putWord8 0 >> putWord8 14
-    get = do
-        0 <- getWord8
-        14 <- getWord8
-        return V14
+  put V14 = putWord8 0 >> putWord8 14
+  get = do
+    0 <- getWord8
+    14 <- getWord8
+    return V14
 
-data Opcode = OpArtDMX | OpArtPoll | OpArtPollReply deriving Show
-
+data Opcode = OpArtDMX | OpArtPoll | OpArtPollReply deriving (Show)
 
 instance Serialize Opcode where
-    put x = putWord16le $ case x of
-        OpArtDMX -> 0x5000
-        OpArtPoll -> 0x2000
-        OpArtPollReply -> 0x2100
-    get = getWord16le >>= \case
-        0x5000 -> return OpArtDMX
-        0x2000 -> return OpArtPoll
-        0x2100 -> return OpArtPollReply
-        x -> fail $ "Unknown opcode " ++ show x
+  put x = putWord16le $ case x of
+    OpArtDMX -> 0x5000
+    OpArtPoll -> 0x2000
+    OpArtPollReply -> 0x2100
+  get =
+    getWord16le >>= \case
+      0x5000 -> return OpArtDMX
+      0x2000 -> return OpArtPoll
+      0x2100 -> return OpArtPollReply
+      x -> fail $ "Unknown opcode " ++ show x
+
 header :: ByteString
 header = "Art-Net\0"
-instance Serialize ArtCommand where
-    put cmd = do
-        putByteString header
-        case cmd of
-            ArtDMX dmx -> put OpArtDMX >> put V14 >> put dmx
-            ArtPoll poll -> put OpArtPoll >> put V14 >> put poll
-            ArtPollReply apr -> put OpArtPoll >> put V14 >> put apr
-    get = do
-        guard . (header ==) =<< getByteString 8
-        opcode <- get
-        V14 <- get
-        case opcode of
-            OpArtPoll -> ArtPoll <$> get
-            OpArtPollReply -> ArtPollReply <$> get 
-            OpArtDMX -> ArtDMX <$> get 
-        
 
+instance Serialize ArtCommand where
+  put cmd = do
+    putByteString header
+    case cmd of
+      ArtDMX dmx -> put OpArtDMX >> put V14 >> put dmx
+      ArtPoll poll -> put OpArtPoll >> put V14 >> put poll
+      ArtPollReply apr -> put OpArtPoll >> put V14 >> put apr
+  get = do
+    guard . (header ==) =<< getByteString 8
+    opcode <- get
+    V14 <- get
+    case opcode of
+      OpArtPoll -> ArtPoll <$> get
+      OpArtPollReply -> ArtPollReply <$> get
+      OpArtDMX -> ArtDMX <$> get
--- src/Server.hs
+++ src/Server.hs
@@ -1,58 +1,69 @@
-{-# LANGUAGE DeriveAnyClass, DeriveGeneric, OverloadedStrings, ScopedTypeVariables #-}
-module Server(main) where
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
+module Server (main) where
+
+import qualified Control.Concurrent.Async as Async
+import qualified Control.Exception as E
+import Control.Monad (forever)
+import qualified Data.Serialize as Ser
+import GHC.Generics (Generic)
+import qualified Lib
 import qualified Network.Socket as NS
 import qualified Network.Socket.ByteString as NSB
-import qualified Control.Exception as E
-import GHC.Generics (Generic)
 import Options.Generic (ParseRecord, getRecord)
-import Control.Monad (forever)
-import qualified Lib
-import qualified Data.Serialize  as Ser
-import qualified Control.Concurrent.Async as Async
 
 serve :: NS.AddrInfo -> (NS.Socket -> IO a) -> IO a
 serve addr go = E.bracket open NS.close go
-    where
+  where
     open = E.bracketOnError (NS.openSocket addr) NS.close $ \sock -> do
-        NS.withFdSocket sock NS.setCloseOnExecIfNeeded
-        NS.setSocketOption sock NS.ReuseAddr 1
-        NS.setSocketOption sock NS.Broadcast 1
-        NS.bind sock $ NS.addrAddress addr
-        return sock
+      NS.withFdSocket sock NS.setCloseOnExecIfNeeded
+      NS.setSocketOption sock NS.ReuseAddr 1
+      NS.setSocketOption sock NS.Broadcast 1
+      NS.bind sock $ NS.addrAddress addr
+      return sock
 
-listen ::  NS.Socket -> IO void
+listen :: NS.Socket -> IO void
 listen sock = forever $ do
-    -- Just in case people use mega-jumbo packets in the future
-    bs <- NSB.recv sock 100000 
-    case Ser.decode bs of
-        Left err -> putStrLn err
-        Right (cmd :: Lib.ArtCommand) -> print cmd
+  -- Just in case people use mega-jumbo packets in the future
+  bs <- NSB.recv sock 100000
+  case Ser.decode bs of
+    Left err -> putStrLn err
+    Right (cmd :: Lib.ArtCommand) -> print cmd
 
 tell :: NS.SockAddr -> NS.Socket -> IO ()
 tell broadcast sock = do
-    len <- NSB.sendTo sock (Ser.encode $ Lib.ArtPoll $ Lib.ArtPoll_ 0 0 Nothing) broadcast
-    print len
+  len <- NSB.sendTo sock (Ser.encode $ Lib.ArtPoll $ Lib.ArtPoll_ 0 0 Nothing) broadcast
+  print len
 
 getAddr :: String -> IO NS.AddrInfo
 getAddr name = do
-    let hints = NS.defaultHints {NS.addrSocketType = NS.Datagram, NS.addrFlags = [NS.AI_PASSIVE]}
-    addrs <- NS.getAddrInfo (Just hints) (Just name) (Just "6454")
-    case addrs of
-        [one] -> return one
-        others -> fail $ "Expected precisely one network address, but found these: " ++ show others
+  let hints =
+        NS.defaultHints
+          { NS.addrSocketType = NS.Datagram,
+            NS.addrFlags = [NS.AI_PASSIVE]
+          }
+  addrs <- NS.getAddrInfo (Just hints) (Just name) (Just "6454")
+  case addrs of
+    [one] -> return one
+    others -> fail $ "Expected precisely one network address, but found these: " ++ show others
 
 data Opts = Opts {broadcastAddr :: String, localAddr :: String} deriving (Generic, Show, ParseRecord)
 
 main :: IO ()
 main = NS.withSocketsDo $ do
-    Opts broadcast_ local_ <- getRecord "Server"
-    broadcast <- getAddr broadcast_
-    local <- getAddr local_
-    let listener = serve broadcast listen
-        teller = serve local (tell $ NS.addrAddress broadcast)
-    Async.withAsync listener $ \listened -> do
-        Async.withAsync teller $ \told -> do
-            (_, err) <- Async.waitAny ["Teller died" <$ told, "Listener died" <$ listened]
-            fail err
-    
+  Opts broadcast_ local_ <- getRecord "Server"
+  broadcast <- getAddr broadcast_
+  local <- getAddr local_
+  let listener = serve broadcast listen
+      teller = serve local (tell $ NS.addrAddress broadcast)
+  Async.withAsync listener $ \listened -> do
+    Async.withAsync teller $ \told -> do
+      (_, err) <-
+        Async.waitAny
+          [ "Teller died" <$ told,
+            "Listener died" <$ listened
+          ]
+      fail err
