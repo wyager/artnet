5d3148fbfb0800fe11e3a90bdf1dc776d48551cc
William Yager <will@yager.io>
2022-09-15 23:39:55 -0400

    fix

---
+++ src/Lib/Data.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE GADTs #-}
+module Lib.Data (Outgoing, fresh, add, finalize ) where
+
+import Prelude hiding (head)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import Data.Serialize (Serialize)
+import qualified Data.Serialize as Ser
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Control.Monad (when, replicateM_)
+import Lib(Data(..))
+
+
+newtype Outgoing = Outgoing (Map Int ByteString)
+
+fresh :: Outgoing
+fresh = Outgoing Map.empty
+
+
+add :: (MonadFail m, Serialize pixel, Traversable f) => Int -> f pixel -> Outgoing -> m Outgoing
+add offset _ _ | offset < 0 = fail "Offset must be >=0"
+add offset pixels (Outgoing chunks) = do
+    case at of
+        Nothing -> return ()
+        Just _conflict -> fail $ "There is already a device starting at offset " ++ show offset
+    case Map.maxViewWithKey below of
+        Nothing -> return ()
+        Just ((belowOff, belowChunk), _) -> when (belowOff + BS.length belowChunk > offset) $
+                fail "The below device's data conflicts with this device."
+    case Map.minViewWithKey above of
+        Nothing -> return ()
+        Just ((aboveOff, _), _) -> when (offset + BS.length encoded > aboveOff) $
+                fail "This would clobber the above device's data"
+    when (offset + BS.length encoded > 512) $
+        fail "There is not enough room to put the data for this device"
+    return $ Outgoing $ Map.insert offset encoded chunks
+    where
+    (below, at, above) = Map.splitLookup offset chunks
+    encoded :: ByteString
+    encoded = Ser.runPut $ mapM_ Ser.put pixels
+        
+
+finalize :: Outgoing -> Data
+finalize (Outgoing chunks) = Data $ Ser.runPut $ go 0 (Map.toAscList chunks)
+    where
+    -- Must be even length
+    go head [] = when (odd head) (Ser.putWord8 0)
+    go head ((offset, chunk):chunks') = do
+        -- Add in filler, as needed
+        replicateM_ (offset - head) (Ser.putWord8 0)
+        Ser.putByteString chunk
+        go (offset + BS.length chunk) chunks'
+        
+        
+
